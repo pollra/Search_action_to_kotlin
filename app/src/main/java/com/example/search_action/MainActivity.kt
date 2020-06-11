3afaa42fb59b45f4be4caf02dda51c53351e2fc8
pollra <pollra32@naver.com>
2020-06-11 21:21:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/search_action/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.search_action
+
+import android.app.SearchManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+
+        val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchItem = menu?.findItem(R.id.search)
+        val searchView = searchItem?.actionView as SearchView // androidx.appcompat.widget.SearchView
+
+        searchView.setSearchableInfo(manager.getSearchableInfo(componentName))
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                searchView.setQuery("", false)
+                searchItem.collapseActionView()
+
+                Toast.makeText(this@MainActivity, "Looking for $query", Toast.LENGTH_SHORT).show()
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Toast.makeText(this@MainActivity, "Looking for $newText", Toast.LENGTH_LONG).show()
+                return false
+            }
+
+        })
+        return true
+    }
+}
